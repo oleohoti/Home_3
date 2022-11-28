3162ec39135635eeb22c6a0ee800cc16034ee6e2
Vladimir <vova.okhotin@mail.ru>
2022-11-28 13:05:03 +0800

    Add Ex19hd

---
+++ Example_019HRD/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите любое целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = num;
+while (x<10)
+{
+     x=x/10;
+}
+int first = num/10000;
+int second = num%10000/1000;
+int fourth = num%100/10;
+int five = num%10;
+if ( first == five && second == fourth) {Console.WriteLine($"число {num} является палиндромом");}
+else {Console.WriteLine($"число {num} не является палиндромом");}
