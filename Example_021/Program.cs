795e4a8bfb6d308a0e36b011e14ebc68fdbb268d
Vladimir <vova.okhotin@mail.ru>
2022-11-28 12:20:18 +0800

    Add Ex21

---
+++ Example_021/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double input(string k, string b)
+{
+    Console.WriteLine($"Введите {k} координату точки {b}");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+try
+{
+    double xA = input("x", "A");
+    double yA = input("y", "A");
+    double zA = input("z", "A");
+    double xB = input("x", "B");
+    double yB = input("y", "В");
+    double zB = input("z", "B");
+
+    double result = Math.Sqrt(Math.Pow((xB - xA), 2) + Math.Pow((yB - yA), 2) + Math.Pow((zB - zA), 2));
+    Console.WriteLine("Расстояние между точками: " + Math.Round(result, 2));
+}
+catch
+{
+    Console.WriteLine("Ошибка выполнения");
+}
