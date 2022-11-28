e928f32e0ebe60c4f774ac0ddfb63a794030cedc
Vladimir <vova.okhotin@mail.ru>
2022-11-28 12:44:51 +0800

    Add Ex23

---
+++ Example_23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= num)
+{
+    int cube = count*count*count;
+    Console.WriteLine($"Таблица кубов: {cube}");
+    count++;
+}
