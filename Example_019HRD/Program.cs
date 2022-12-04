894b0b4f315dee23e909c8aa41411078bfe8b9b0
Vladimir <vova.okhotin@mail.ru>
2022-12-04 22:22:25 +0800

    Ex19pro

--- Example_019HRD/Program.cs
+++ Example_019HRD/Program.cs
@@ -4,37 +4,57 @@ int num = Convert.ToInt32(Console.ReadLine());
 int size = Length(num);
 int num1 = num;
 int size1 = size;
-int Length (int num) // посчитали кол-во элементов
+int Length(int num) // посчитали кол-во элементов
 {
-     int len= 0;
-     while(num!=0){num=num/10; len++;}
-     return len;
+    int len = 0;
+    while (num != 0) { num = num / 10; len++; }
+    return len;
 }
 int[] ar(int size) // Функция цикла складирования по элеметно задом наперед числа num
 {
     int[] array = new int[size];
     for (int i = 0; i < size; i++)
     {
-        array[i] = num%10;
-        num=num/10;
+        array[i] = num % 10;
+        num = num / 10;
     }
     return array;
 }
 int[] ar2(int[] array1, int size1) // Функция перевернутого первогоо цикла
 {
-     
+
     int[] array = new int[size1];
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = array1[size1-1];
-        size1=size1-1;
+        array[i] = array1[size1 - 1];
+        size1 = size1 - 1;
     }
     return array;
 }
+string mes(int[] array1, int[] array2) // Функция сравнения
+{
+    bool ans = true;
+    string str = "";
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i] != array2[i])
+        {
+            ans = false;
+        }
+    }
+    
+    if (ans == true)
+    {
+        str = "Число является палиндромом";
+    }
 
+    if (ans == false)
+    {
+        str = "Число не является палиндромом";
+    }
+    return str;
+}
 int[] array1 = ar(size);// создали цикл array1 для складирования поэлементно числа num
 int[] array2 = ar2(array1, size1); // создан перевернутый цикл
-if(array1.SequenceEqual(array2)) Console.WriteLine($"Число {num1} является палиндромом");
-else Console.WriteLine($"Число {num1} не является палиндромом");
-// if(array1.SequenceEqual(array2)) Console.WriteLine($"Число {num1} является палиндромом");
-// else Console.WriteLine($"Число {num1} не является палиндромом");
+Console.WriteLine(mes(array1, array2));
+
