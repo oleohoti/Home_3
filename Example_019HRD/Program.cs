738406f39b5b676652aa83088805ee6634df6b4a
Vladimir <vova.okhotin@mail.ru>
2022-12-04 14:04:48 +0800

    Ex19pro

--- Example_019HRD/Program.cs
+++ Example_019HRD/Program.cs
@@ -6,16 +6,43 @@ int Length (int num) // посчитали кол-во элементов
      while(num!=0){num=num/10; len++;}
      return len;
 }
-Console.WriteLine(Length(num)); 
-int num1 = num;
-int i = 0;
-int j = 0;
-int[] array1 = new int[Length(num)];// создали цикл array1 для складирования поэлементно числа num
-for ( i=0; i<Length(num); i++) {array1[i]=num1%10; num1 = num1/10;}
+// Console.WriteLine(Length(num)); 
 int size = Length(num);
-int[] array2 = new int[Length(num)];
-for ( j=0; j<Length(num); j++) {array2[j]=array1[size]; size=size-1;}
-while (i<size) {if (array1[i]!=array2[j]){Console.WriteLine("число палиндримом не является");} else {i++;j++;} if(array1[size] == array2[size]){Console.WriteLine("число является ");}}
+int num1 = num;
+int size1 = size;
+int[] ar(int size) // Функция цикла складирования по элеметно задом наперед числа num
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = num%10;
+        num=num/10;
+    }
+    return array;
+}
+int[] ar2(int[] array1, int size1) // Функция перевернутого первогоо цикла
+{
+     
+    int[] array = new int[size1];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array1[size1-1];
+        size1=size1-1;
+    }
+    return array;
+}
+
+int[] array1 = ar(size);// создали цикл array1 для складирования поэлементно числа num
+int[] array2 = ar2(array1, size1); // создан перевернутый цикл
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"{array1[i]} ");
+}
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"{array2[i]} ");
+}
 
 
 
