2e59c28953b7b4a47cfc5487f927d8afd85d4f56
Vladimir <vova.okhotin@mail.ru>
2022-12-04 10:38:21 +0800

    Ex21

--- Example_021/Program.cs
+++ Example_021/Program.cs
@@ -12,12 +12,12 @@ try
     double zA = input("z", "A");
     double xB = input("x", "B");
     double yB = input("y", "В");
-    double zB = input("z", "B");
+    double zB = input("z", "B");// N=Math.Row((kB),2)
 
     double result = Math.Sqrt(Math.Pow((xB - xA), 2) + Math.Pow((yB - yA), 2) + Math.Pow((zB - zA), 2));
     Console.WriteLine("Расстояние между точками: " + Math.Round(result, 2));
 }
 catch
 {
-    Console.WriteLine("Ошибка выполнения");
+    Console.WriteLine("Ошибка выполнения");// A (3,6,8); B (2,1,-7), -> 15.84
 }
