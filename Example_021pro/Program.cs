47d9dcb44c43c8ec8784589ad09e99f39cb8d193
Vladimir <vova.okhotin@mail.ru>
2022-12-04 22:33:25 +0800

    Ex21pro

--- Example_021pro/Program.cs
+++ Example_021pro/Program.cs
@@ -1,29 +1,35 @@
-﻿    Console.Write("Введите колличество пространств, в которых находятся точки: ");
-    int pros = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Введите колличество пространств, в которых находятся точки: ");
+int pros = Convert.ToInt32(Console.ReadLine());
 
-    int pros1 = pros;
-    int pros2 = pros;
+int pros1 = pros;
+int pros2 = pros;
 
-    double [] A = new double [pros1]; // определяем размерность двух массивов
-    double [] B = new double [pros2];
-    
-    for (int i=0; i<=pros1-1;i++) // цикл заполнения массивов значениями
+double []array (int pros)
+{
+    double [] A = new double [pros];
+    for (int i=0; i<=pros-1;i++) // цикл заполнения массивов значениями
     {
-        Console.Write($"Введите координаты {i+1}-ого пространства точки А ");
-        A[i] = Convert.ToDouble(Console.ReadLine());
+    Console.Write($"Введите координаты {i+1}-ого пространства точки А ");
+    A[i] = Convert.ToDouble(Console.ReadLine());
     }
+    return A;
+}
+double [] massiv=array(pros);
 
-    double [] raz = new double [pros]; // размерность массива который будет заполняться разницой в квадрате для дальнейшего вывода из под корня
-    double sum = 0; 
+for (int i = 0; i < massiv.Length; i++)
+{
+    Console.WriteLine(massiv[i]);
+}
+    // double [] raz = new double [pros]; // размерность массива который будет заполняться разницой в квадрате для дальнейшего вывода из под корня
+    // double sum = 0; 
 
-    for (int j=0; j<=pros-1;j++) // массив для заполнения разницы в квадрате и суммирования (формула без корня)
-    {
-        raz [j] = Math.Pow(B[i]-A[i],2); 
-        sum=sum+raz[j];
-        return sum;
-        Console.WriteLine($"{raz[j]}");
-        i++; 
-    }
+    // for (int j=0; j<=pros-1;j++) // массив для заполнения разницы в квадрате и суммирования (формула без корня)
+    // {
+    //     raz [j] = Math.Pow(B[i]-A[i],2); 
+    //     sum=sum+raz[j];
+    //     Console.WriteLine($"{raz[j]}");
+    //     i++; 
+    // }
     
-    double distance = Math.Sqrt(sum); // находим дистанцию финальной формулой
-    Console.WriteLine($"Расстояние от точки А до точки B в {i}-ом пространсве равно: {distance}");
+    // double distance = Math.Sqrt(sum); // находим дистанцию финальной формулой
+    // Console.WriteLine($"Расстояние от точки А до точки B в {i}-ом пространсве равно: {distance}");
